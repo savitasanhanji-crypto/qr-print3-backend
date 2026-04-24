04dc08ec2d208ea2a74eeb02b9fce1f386d9c47c
SavitaHanji <savita.hanji@mitvpu.ac.in>
2026-04-24 11:45:21 +0530

    fix conductor model collection name to conductors

--- models/Conductor.js
+++ models/Conductor.js
@@ -1,12 +1,10 @@
-const mongoose = require("mongoose");
-
+﻿const mongoose = require("mongoose");
 const conductorSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     batch_no: { type: String, required: true, unique: true },
     password: { type: String, required: true }
   },
-  { collection: "Conductor" } // 👈 Forces collection name
+  { collection: "conductors" }
 );
-
 module.exports = mongoose.model("Conductor", conductorSchema);
