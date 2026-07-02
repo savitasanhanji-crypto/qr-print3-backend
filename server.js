32621b69edc381ae5cbb13ba0faeb4be4d5e0b3c
SavitaHanji <savita.hanji@mitvpu.ac.in>
2026-07-03 01:58:56 +0530

    revert otp password reset

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const busRoutes = require("./routes/busRoutes");
 const ticketRoute = require("./routes/ticketRoutes");
 const posRoutes = require("./routes/pos");
 const fareRoutes = require("./routes/fareRoutes");
-const otpRoutes = require("./routes/otpRoutes");
+
 
 const app = express();
 
@@ -54,4 +54,3 @@ app.use("/conductor", conductorRoutes);
 app.use("/api/passes", passRoutes);
 app.use("/api/pos", posRoutes);
 app.use("/api", fareRoutes);
-app.use("/conductor", otpRoutes);
