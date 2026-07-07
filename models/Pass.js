f6077fef3b77c2996ef0f9f8d266105c32135404
SavitaHanji <savita.hanji@mitvpu.ac.in>
2026-07-07 23:52:46 +0530

    fix Pass model collection name to passes

--- models/Pass.js
+++ models/Pass.js
@@ -1,11 +1,8 @@
-const mongoose = require("mongoose");
-
+﻿const mongoose = require("mongoose");
 const passSchema = new mongoose.Schema({
   passName: { type: String, required: true },
-  concession: { type: String, required: true },
+  concession: { type: Number, default: 0 },
   description: { type: String }
-}, { collection: "Pass" }); // <-- explicitly set collection name
-
+}, { collection: "passes" });
 const Pass = mongoose.model("Pass", passSchema);
-
 module.exports = Pass;
