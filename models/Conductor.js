c6ea238072168f99b540f6946bbf171b1df174ad
SavitaHanji <savita.hanji@mitvpu.ac.in>
2026-07-07 21:59:23 +0530

    fix phone_no field for OTP

--- models/Conductor.js
+++ models/Conductor.js
@@ -1,10 +1,11 @@
 ﻿const mongoose = require("mongoose");
 const conductorSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    batch_no: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    Contact: { type: String, default: "" },
+    name: { type: String },
+    batch_no: { type: String },
+    password: { type: String },
+    phone_no: { type: String, default: "" },
+    type: { type: String, default: "" },
   },
   { collection: "Conductor" }
 );
